b93e88495533361a38c84cdab9733fff28260ff2
YotsaponDev <boss.yotsapon@gmail.com>
2019-11-23 16:33:30 +0700

    Client responsive UI #1

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
     <div class="top-nav">
       <div class="icon-bar">
-        <span style="font-size: 20px; color: #99abb4 !important;">
+        <span v-show="iconBarValue" v-on:click="ClickIconBar()" style="font-size: 20px; color: #99abb4 !important; cursor:pointer;">
+          <i class="fas fa-times"></i>
+        </span>
+        <span v-show="!iconBarValue" v-on:click="ClickIconBar()" style="font-size: 20px; color: #99abb4 !important; cursor:pointer;">
           <i class="fas fa-bars"></i>
         </span>
       </div>
     </div>
-    <div class="left-side">
+    <div :class="classLeftSide">
        <!-- <router-link to="/">Home</router-link> |
         <router-link to="/about">About</router-link> -->
       
     </div>
-    <div class="content">
+    <div :class="classContent">
       <router-view/> 
     </div>
   </div>
@@ -22,6 +25,9 @@
 export default {
   data() {
     return {
+      classLeftSide: 'left-side',
+      classContent: 'content',
+      iconBarValue: false,
       window: {
         width: 0,
         height: 0
@@ -36,18 +42,44 @@ export default {
     window.removeEventListener('resize', this.handleResize)
   },
   methods: {
+    ClickIconBar(){
+      this.iconBarValue = this.iconBarValue == true ? false : true
+      if(this.window.width >= 992){
+        if(this.iconBarValue){
+          this.classLeftSide = 'left-side clickToMD'
+          this.classContent = 'content clickToMD'
+        }else{
+          this.classLeftSide = 'left-side'
+          this.classContent = 'content'
+        }
+      }else if(this.window.width >= 768){
+        if(this.iconBarValue){
+          this.classLeftSide = 'left-side clickToLG'
+          this.classContent = 'content'
+        }else{
+          this.classLeftSide = 'left-side'
+          this.classContent = 'content'
+        }
+      }else{
+        if(this.iconBarValue){
+          this.classLeftSide = 'left-side clickToLG'
+          this.classContent = 'content'
+        }else{
+          this.classLeftSide = 'left-side'
+          this.classContent = 'content'
+        }
+      }
+    },
     handleResize() {
+      this.iconBarValue = false
       this.window.width = window.innerWidth;
       this.window.height = window.innerHeight;
 
       var body = document.body;
+
+        this.classLeftSide = 'left-side'
+        this.classContent = 'content'
       
-      console.log(this.window.width);
-      if(this.window.width > 1009){
-        body.classList.remove("small-device");
-      }else{
-        body.classList.add("small-device");
-      }
       
     }
   }
